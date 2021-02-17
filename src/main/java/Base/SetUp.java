3df33c2739865aacdcb0994afa852f1bad8f190e
rajendra8 <Rajendra8.sahu@gmail.com>
2021-02-17 23:47:40 +0530

    commiting and pushing after changing into 2 file

--- src/main/java/Base/SetUp.java
+++ src/main/java/Base/SetUp.java
@@ -4,5 +4,6 @@ public class SetUp {
 
     public static void main(String [] arg){
         System.out.println("here is the Setup class started");
+        System.out.println("here is the Setup class started");
     }
 }
--- src/test/java/Test/Testclass.java
+++ src/test/java/Test/Testclass.java
@@ -4,5 +4,6 @@ public class Testclass {
 
     public static void main(String [] arg){
         System.out.println("here is the test class begun");
+        System.out.println("here is the test class begun");
     }
 }
