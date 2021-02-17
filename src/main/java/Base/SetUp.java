a27e97a9682e68244886d48d6e38203c388e7bc3
rajendra8 <Rajendra8.sahu@gmail.com>
2021-02-17 17:20:13 +0530

    Commiting the new java file

---
+++ src/main/java/Base/SetUp.java
@@ -0,0 +1,8 @@
+package Base;
+
+public class SetUp {
+
+    public static void main(String [] arg){
+        System.out.println("here is the Setup class started");
+    }
+}
---
+++ src/test/java/Test/Testclass.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class Testclass {
+
+    public static void main(String [] arg){
+        System.out.println("here is the test class begun");
+    }
+}
