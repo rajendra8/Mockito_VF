6eddd15dc8de327624b82013336a9e52d4228b27
rajendra8 <Rajendra8.sahu@gmail.com>
2021-02-18 10:53:40 +0530

    18/feb/2021 v1 commit

--- src/main/java/BasePackage/BaseClass.java
+++ src/main/java/BasePackage/BaseClass.java
@@ -4,5 +4,6 @@ public class BaseClass {
 
     public static void main(String [] arg){
         System.out.println("Git demo V1 project");
+        System.out.println("Git demo V1 project");
     }
 }
